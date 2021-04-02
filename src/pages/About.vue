a5d032478bac3957559b8fd97c2a06eebe8234f4
Ben Soutendijk <bensoutendijk@gmail.com>
2021-04-01 19:55:01 -0500

    initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout>
+    <h1>About</h1>
+  </layout>
+</template>
+
+<script>
+import Layout from '@/layouts/default'
+
+export default {
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import Layout from '@/layouts/default'
+
+export default {
+  name: 'Home',
+  created () {
+    this.$emit('update:layout', Layout)
+  }
+}
+</script>
+
+<style>
+
+</style>
