516ef3338515c90d7a9ff4efa5daced1f7f95efb
bensoutendijk <bensoutendijk@gmail.com>
2021-04-03 23:01:31 -0500

    downgrade Vue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import { createApp } from "vue";
-import "vue-trix";
+import Vue from "vue";
 
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
+import router from "./router";
 import "@/assets/styles/main.css";
 
-createApp(App).use(store).use(router).mount("#app");
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
-import { createStore } from "vuex";
+import Vue from "vue";
+import Vuex from "vuex";
+
 import auth from "./auth";
 import pages from "./pages";
 
-export default createStore({
+Vue.use(Vuex);
+
+export default new Vuex.Store({
   modules: {
     auth,
     pages,
